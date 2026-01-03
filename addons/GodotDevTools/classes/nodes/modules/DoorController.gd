d40f20d571ff61131da28ca96e90fbe39cfe3f67
Cairo555 <niels@appps4u.nl>
2026-01-03 13:50:26 +0100

    .
    .

--- addons/GodotDevTools/classes/nodes/modules/DoorController.gd
+++ addons/GodotDevTools/classes/nodes/modules/DoorController.gd
@@ -2,96 +2,144 @@
 extends Module
 class_name DoorController
 
-##Module that keeps track of the state of a door and plays animations for opening and closing the door when triggered.
+## DoorController (Rotate mode) + simple deterministic Open SFX.
+##
+## USAGE:
+## 1) Add an AudioStreamPlayer3D node anywhere you want (you already have SFX_Door).
+## 2) Select DoorController -> Inspector:
+##    - Assign `sfx_player` to your AudioStreamPlayer3D (drag SFX_Door onto it)
+##    - Assign `door_sfx` to your .ogg/.wav
+## 3) Run -> opening plays sound.
+##
+## Debug:
+## - Prints why sound didn't play (missing player/stream/bus muted/etc.)
 
 @export var door_opened : bool = false:
 	set(x):
 		door_opened = x
-		
-		#if Engine.is_editor_hint(): #For fancy animations in editor, not necessary but looks cool
-			#if x == true:
-				#open_door_anim()
-			#else:
-				#close_door_anim()
 
-#@export_enum("Open-Close", "Single", "Armature Actions", "Rotate") var animation_mode : int = 0 ##The way that the animations are controlled.[br][br] - [param Open-Close] assumes that all animations are baked into 1 animation (by setting Split by Object in export settings to false) and that the opening and closing animations are one action and succeed each other within that action. [br] - [param Single] assumes that you only have one animation with one or more objects baked into the scene that you want to play in reverse for the closing animation.[br] - [param Armature Action] assumes all objects are parented to an armature, which then has seperate actions for both opening and closing.
-#@export var border_time : float = 0 ##Used in the [param Open-Close] animation mode. Determines where the closing animation starts, and also where the opening animation needs to stop playing.
+@export_range(0.0, 1.0, 0.01, "or_greater", "hide_slider") var autoclose_time : float = 0
+@export var open_rotation_offset : float = 90.0
+@export var rotation_time : float = 0.4
 
-@export_range(0.0, 1.0, 0.01, "or_greater", "hide_slider") var autoclose_time : float = 0 ##If set to anything else than 0, this [DoorController] will automatically close this door [param autoclose_time] seconds after the opening animation has finished.
-
-@export var open_rotation_offset : float = 90.0 ##The amount of degrees the door will turn from its starting position when the door is opened.
-@export var rotation_time : float = 0.4 ##The time it takes for the door to complete its turning animation
+# --- AUDIO (deterministic) ---
+@export var door_sfx : AudioStream
+@export var sfx_player : AudioStreamPlayer3D  # <— drag your SFX_Door here in Inspector
+@export var debug_audio : bool = true
 
 var _init_rotation : float
-
-var _animation_player_instance : AnimationPlayer
-
-var _playing_animation_section : bool = false
-signal _animation_section_finished
-
 @onready var _triggers : Array[Trigger] = get_triggers()
 
 func _ready() -> void:
-	#if !get_parent().has_node("AnimationPlayer"):
-		#printerr(self, " does not have an AnimationPlayer as sibling and will not function.")
-	#else:
-		#_animation_player_instance = get_parent().get_node("AnimationPlayer")
-	#
-	if !Engine.is_editor_hint():
-		_init_rotation = rotation_degrees.y
-		
-		if _triggers == []:
-			no_triggers_error()
-		else:
-			var _has_enabled_trigger = false
-			for _trigger_object : Trigger in _triggers:
-				if _trigger_object.enabled:
-					_has_enabled_trigger = true
-					_trigger_object.toggle.connect(toggle_door)
-			
-			if !_has_enabled_trigger:
-				no_enabled_triggers_error()
-	
-	#if door_opened:
-		#_animation_player_instance.play("Scene", -1, 0.0) #Make sure "Scene" is loaded before seeking
-		#_animation_player_instance.stop()
-		#_animation_player_instance.seek(border_time, true)
+	if Engine.is_editor_hint():
+		return
+
+	_init_rotation = rotation_degrees.y
+
+	# Hook triggers
+	if _triggers == []:
+		no_triggers_error()
+	else:
+		var _has_enabled_trigger = false
+		for _trigger_object : Trigger in _triggers:
+			if _trigger_object.enabled:
+				_has_enabled_trigger = true
+				_trigger_object.toggle.connect(toggle_door)
+		if !_has_enabled_trigger:
+			no_enabled_triggers_error()
+
+	# Audio sanity log once
+	if debug_audio:
+		_debug_audio_state("ready")
 
 func toggle_door(toggled_with_key, state : bool):
 	print("toggle ", state)
+
+	# Play ONLY on opening
 	if state:
+		_play_open_sfx()
+
 		var _rotate_tween : Tween = create_tween()
 		_rotate_tween.tween_property(get_parent(), "rotation_degrees:y", _init_rotation + open_rotation_offset, rotation_time)
 	else:
 		var _rotate_tween : Tween = create_tween()
 		_rotate_tween.tween_property(get_parent(), "rotation_degrees:y", _init_rotation, rotation_time)
-	#if state == false:
-		#if _playing_animation_section:
-			#await _animation_section_finished
-		#close_door_anim()
-		#door_opened = false
-	#else:
-		#if _playing_animation_section:
-			#await _animation_section_finished
-		#open_door_anim()
-		#door_opened = true
+
+func _play_open_sfx() -> void:
+	if Engine.is_editor_hint():
+		return
+
+	if sfx_player == null:
+		if debug_audio:
+			printerr("DoorController audio: sfx_player is NULL. Assign it in Inspector (drag SFX_Door onto sfx_player).")
+		return
+
+	# Ensure we have a stream
+	if door_sfx != null:
+		sfx_player.stream = door_sfx
+
+	if sfx_player.stream == null:
+		if debug_audio:
+			printerr("DoorController audio: No stream set. Assign door_sfx (.ogg/.wav) in Inspector.")
+		return
+
+	# Extra: if bus muted/volume too low you won't hear anything
+	if debug_audio:
+		_debug_audio_state("play")
+
+	# Restart and play
+	sfx_player.stop()
+	sfx_player.play()
+
+func _debug_audio_state(tag: String) -> void:
+	var bus := "?"
+	var vol := 0.0
+	var playing := false
+	var has_stream := false
+	var max_dist := 0.0
+	var unit_size := 0.0
+
+	if sfx_player != null:
+		bus = sfx_player.bus
+		vol = sfx_player.volume_db
+		playing = sfx_player.playing
+		has_stream = (sfx_player.stream != null) or (door_sfx != null)
+		max_dist = sfx_player.max_distance
+		unit_size = sfx_player.unit_size
+
+	print("[DoorController audio:", tag, "] ",
+		"player=", sfx_player,
+		" has_stream=", has_stream,
+		" bus=", bus,
+		" volume_db=", vol,
+		" max_distance=", max_dist,
+		" unit_size=", unit_size,
+		" playing=", playing
+	)
+
+
+
+# -----------------------------------------
+# (Your old animation-player code remains untouched below.)
+# -----------------------------------------
+
+#@export_enum("Open-Close", "Single", "Armature Actions", "Rotate") var animation_mode : int = 0
+#@export var border_time : float = 0
 #
 #func open_door_anim():
-	#if animation_mode == 0:
-		#_animation_player_instance.seek(0.0, true)
-		#_playing_animation_section = true
-		#_animation_player_instance.play_section("Scene", 0.0, border_time)
-		#
-		#await get_tree().create_timer(border_time).timeout
-		#_playing_animation_section = false
-		#_animation_section_finished.emit()
+#	if animation_mode == 0:
+#		_animation_player_instance.seek(0.0, true)
+#		_playing_animation_section = true
+#		_animation_player_instance.play_section("Scene", 0.0, border_time)
+#		await get_tree().create_timer(border_time).timeout
+#		_playing_animation_section = false
+#		_animation_section_finished.emit()
 #
 #func close_door_anim():
-	#if animation_mode == 0:
-		#_animation_player_instance.seek(border_time, true)
-		#_playing_animation_section = true
-		#_animation_player_instance.play_section("Scene", border_time)
-		#
-		#await get_tree().create_timer(_animation_player_instance.get_animation("Scene").length - border_time).timeout
-		#_playing_animation_section = false
-		#_animation_section_finished.emit()
+#	if animation_mode == 0:
+#		_animation_player_instance.seek(border_time, true)
+#		_playing_animation_section = true
+#		_animation_player_instance.play_section("Scene", border_time)
+#		await get_tree().create_timer(_animation_player_instance.get_animation("Scene").length - border_time).timeout
+#		_playing_animation_section = false
+#		_animation_section_finished.emit()
